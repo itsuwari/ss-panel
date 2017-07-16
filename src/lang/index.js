502defea0e7686a70403b3334b17a223e19a014a
Fuuko Wu <fraxinasw@gmail.com>
2017-07-16 16:14:31 +0900

    Update index.js

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,11 +1,9 @@
 import lang from './vue-i18n-locales.generated'
 
 export const langs = {
-    en: "🇺🇸English",
-    zh_cn: "🇨🇳简体中文",
+    en: "English"
 };
 
 export  const Locales = {
-    en: lang.en,
-    zh_cn: lang.zh_cn, // @todo
+    en: lang.en
 };
